6322ef717d4b6b285d231d265b1346d5dec5e1e8
Vera <veramagical@mail.ru>
2018-07-01 17:47:52 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,14 +1,14 @@
 #ifndef PARSER_H_INCLUDED
 #define PARSER_H_INCLUDED
 
-// Синтаксис
-// Типы слов
+// РЎРёРЅС‚Р°РєСЃРёСЃ
+// РўРёРїС‹ СЃР»РѕРІ
 #define KEYWORD 1
 #define OPERATOR 3
 #define VARIABLE 5
 #define CONSTANT 7
 
-// Формы слов
+// Р¤РѕСЂРјС‹ СЃР»РѕРІ
 #define LOOP 2
 #define DO 4
 #define END 6
@@ -17,13 +17,13 @@
 void initWord();
 
 struct words {
-    char name[100];             // Возьмем например длинной 100
+    char name[100];             // Р’РѕР·СЊРјРµРј РЅР°РїСЂРёРјРµСЂ РґР»РёРЅРЅРѕР№ 100
     int type;
     int form;
 };
 
 struct keywords {
-    char name[5];               // Самое длинное = 4 + 1 = \0
+    char name[5];               // РЎР°РјРѕРµ РґР»РёРЅРЅРѕРµ = 4 + 1 = \0
     int form;
 };
 
